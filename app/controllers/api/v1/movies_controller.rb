232f042d392b92a30bbd5d74e40d6aa2fbde525b
mbronek7 <mbronek7@gmail.com>
2020-11-21 17:44:36 +0100

    Add movies controller

---
+++ app/controllers/api/v1/movies_controller.rb
@@ -0,0 +1,29 @@
+module Api
+  module V1
+    class MoviesController < Api::BaseController
+      def index
+        get_movies
+      end
+
+      def show
+        render_json(serializer, movie)
+      end
+
+      private
+
+      def serializer
+        Api::V1::MovieSerializer
+      end
+
+      def movie
+        @movie ||= Movie.find(params[:id])
+      end
+
+      def get_movies
+        page = params[:page] || 1
+        render_json(serializer, Movie.all.page(page))
+      end
+
+    end
+  end
+end
