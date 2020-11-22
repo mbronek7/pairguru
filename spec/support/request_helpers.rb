f935d3131f96c6d78939d77f0ff7bbb703696ec3
mbronek7 <mbronek7@gmail.com>
2020-11-22 02:48:41 +0100

    Add devise authenticate helpers to rspec

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,5 +1,26 @@
 module RequestHelpers
+  include Warden::Test::Helpers
+
+  def self.included(base)
+    base.before(:each) { Warden.test_mode! }
+    base.after(:each) { Warden.test_reset! }
+  end
+
+  def sign_in(resource)
+    login_as(resource, scope: warden_scope(resource))
+  end
+
+  def sign_out(resource)
+    logout(warden_scope(resource))
+  end
+
   def json_response
     @json_response ||= JSON.parse(response.body)
+  end
+
+  private
+
+  def warden_scope(resource)
+    resource.class.name.underscore.to_sym
   end
 end
