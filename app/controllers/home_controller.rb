a26316cf0f6148684a9c926ea55311d362bbeb81
mbronek7 <mbronek7@gmail.com>
2020-11-23 03:23:36 +0100

    Add top commenters in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,7 @@
 class HomeController < ApplicationController
   def welcome; end
+
+  def top_commenters
+    @users = Users::TopCommentersQuery.call
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "home#welcome"
+  get "/commenters", to: "home#top_commenters"
   resources :genres, only: :index do
     member do
       get "movies"
