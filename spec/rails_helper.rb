fbbddc9ab575dd0c85e8e50a02e269a146cf5320
mbronek7 <mbronek7@gmail.com>
2020-11-23 02:39:23 +0100

    Fix requiring support files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,7 +24,7 @@ SimpleCov.start "rails"
 # require only the support files necessary.
 #
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| p f; require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
